3db45af8fadee2d4d094176a7f083c510aef03d9
Andres Gonzalez <32281805+andres2392@users.noreply.github.com>
2026-04-24 16:39:28 -0400

    feat: add styling to pages

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,40 +1,45 @@
-import MovieCard from "../components/MovieCard"
-import {useState} from "react"
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
+import "../css/Home.css"
 
-function Home(){
-const [searchQuery, setSearchQuery] = useState("");
+function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
 
-    const movies = [
-        {id:1, title:"John Wick", release_date:"2020"},
-        {id:2, title:"Anaconda", release_date:"2024"},
-        {id:3, title:"Deep Blue", release_date:"2023"}
-    ]
+  const movies = [
+    { id: 1, title: "John Wick", release_date: "2020" },
+    { id: 2, title: "Anaconda", release_date: "2024" },
+    { id: 3, title: "Deep Blue", release_date: "2023" },
+  ];
 
-    const handleSearch = (e) => {
-        e.preventDefault()
-        alert(searchQuery)
-        setSearchQuery("----")
-    }
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+    setSearchQuery("");
+  };
 
-    return (
-        <div className="home">
-            <form onSubmit={handleSearch} className="search-form">
-                <input 
-                    type="text" 
-                    placeholder="Search for movies.." 
-                    className="search-input"
-                    value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)}
-                />
-                <button type="submit" className="search-button">Search</button>     
-             </form>
-            <div className="movies-grip">
-                {movies.map((movie) => (
-                    <MovieCard movie={movie} key={movie.id}/>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies.."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
